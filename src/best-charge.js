d04a9e1bc8643028c4df75e8b58c78d4430f54d1
pandajiangbao <pandawsh@icloud.com>
2019-07-11 01:15:54 +0800

    finish

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,25 +1,38 @@
 function bestCharge(selectedItems) {
-  let discount=0
-  let total=0
-  let result=`
-  ============= 订餐明细 =============
-  `
-  selectedItems.forEach(element=>{
-    let list =element.split(' x ')
+  let discount = 0
+  let promotionNames = []
+  let total = 0
+  let result = `\n============= 订餐明细 =============\n`
+  selectedItems.forEach(element => {
+    let list = element.split(' x ')
     loadAllItems().forEach(item => {
-      if (list[0]===item.id) {
-        result+=`${item.name}  x ${list[1]} = ${item.price*Number(list[1])}元\n`
-        if(element.id=='ITEM0001'||element.id=='ITEM0022'){
-          
+      if (list[0] === item.id) {
+        result += `${item.name} x ${list[1]} = ${item.price * Number(list[1])}元\n`
+        total += item.price * Number(list[1])
+        if (loadPromotions()[1].items.includes(list[0])) {
+          discount += item.price * Number(list[1]) / 2
+          promotionNames.push(item.name)
         }
       }
     })
-    result+=`-----------------------------------
-    使用优惠:
-    ${}，省${}元
-    -----------------------------------
-    总计：${}元
-    ===================================`
   })
+  if ((total >= 30 && discount > 6) || (total < 30 && discount > 0)) {
+    let promotionItem = promotionNames.reduce((acc, cur, index) => {
+      acc += index === 0 ? cur : '，' + cur
+      return acc
+    }, '')
+    var promotionType = loadPromotions()[1].type
+    var promotionStr = `使用优惠:\n${promotionType}(${promotionItem})，省${discount}元\n-----------------------------------\n`
+  }
+  else if (total >= 30 && discount <= 6) {
+    discount = 6
+    var promotionType = loadPromotions()[0].type
+    var promotionStr = `使用优惠:\n${promotionType}，省${discount}元\n-----------------------------------\n`
+  }
+  else if (total < 30 && discount === 0) {
+    discount = 0
+    var promotionStr = ``
+  }
+  result += `-----------------------------------\n` + promotionStr +`总计：${total-discount}元\n===================================`
   return result/*TODO*/
 }
